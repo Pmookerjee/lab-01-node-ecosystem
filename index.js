40d2c9ae286bf628199fbdda116f6c2e7dd1906a
Pmookerjee <pmookerjee@gmail.com>
2017-09-25 23:25:21 -0700

    Added the index.js first and then the print-file. All is working.

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const readFiles = require('./lib/print-files.js');
+
+let printFile = module.exports = function(err, data) {
+  if(err) {console.log(err); }
+  console.log(data.toString());
+};
+
+(() => {
+  if(process.argv < 3) {
+    console.log('USAGE: node ' + process.argv[1] + ' FILENAME');
+    process.exit(1);
+  }
+  for(var i = 2; i < process.argv.length; i++) {
+    readFiles(process.argv[i], printFile);
+  }
+})();
